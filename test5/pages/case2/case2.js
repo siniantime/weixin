7fd4f4ccd0ebade49f11a3103ffa1fce10ecc8d7
Sup_ <1099982811@qq.com>
2017-01-16 08:16:39 +0800

    Add files via upload

---
+++ test5/pages/case2/case2.js
@@ -0,0 +1,34 @@
+Page({
+	data:{
+		indicatorDots:true,
+		interval:4000,
+		duration:1000,
+		imgs:[
+			'/imgs/1.jpg',
+			'/imgs/2.jpg',
+			'/imgs/3.jpg',
+			'/imgs/4.jpg',
+		]
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	}
+});		
---
+++ test5/pages/case3/case3.js
@@ -0,0 +1,29 @@
+Page({
+	data:{
+		
+	},
+	lower:function(){
+		console.log('lower');
+	},
+	onLoad:function(options){
+		
+	},
+	onReady:function(){
+		
+	},
+	onShow:function(){
+		
+	},
+	onHide:function(){
+		
+	},
+	onUnload:function(){
+		
+	},
+	onPullDownRefresh:function(){
+		
+	},
+	onReachBottom:function(){
+		
+	}
+})		
