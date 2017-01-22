aa7e584fb20a493d6a023122690cbddf8d21104f
Sup_ <1099982811@qq.com>
2017-01-22 15:13:34 +0800

    Add files via upload

---
+++ coffee/pages/index/index.js
@@ -0,0 +1,30 @@
+Page({
+  data:{
+    imgUrls:[
+      "img/coffee.jpg",
+      "img/milkTea.jpg",
+      "img/frappuccino.jpg"
+    ]
+  },
+  onLoad:function(options){
+    
+  },
+  onReady:function(){
+    
+  },
+  onShow:function(){
+    
+  },
+  onHide:function(){
+    
+  },
+  onUnload:function(){
+    
+  },
+  onPullDownRefresh:function(){
+    
+  },
+  onReachBottom:function(){
+    
+  }
+})    
